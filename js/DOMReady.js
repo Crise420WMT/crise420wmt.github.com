7969080a19d143acd4101196a7a78a931c4c2b4d
crise420wmt <crise420wmt@163.com>
2016-07-23 15:24:23 +0800

    mywebsit

---
+++ js/DOMReady.js
@@ -0,0 +1,13 @@
+function DOMReady(fn){
+    if(document.addEventListener){
+        document.addEventListener('DOMContentLoaded',function(){
+            fn&&fn();
+        },false)
+    }else{
+        document.onreadystatechange=function(){
+            if(document.readyState=='complete'){
+                fn&&fn();
+            }
+        };
+    }
+}
